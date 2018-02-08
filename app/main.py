c64e31ae8cffc9648911cf701a94d27a7477c1bd
Helen King <helenking029@gmail.com>
2018-02-07 17:55:57 -0800

    Updating Move
    testing updated "move"

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import random
 
 @bottle.route('/')
 def index():
-    return 'I am a snake.'
+    return 'This is a snake.'
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -26,7 +26,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#33CA7F',
+        'color': '#00FF00',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'battlesnake-python'
@@ -40,13 +40,22 @@ def move():
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
 
+    #return {
+    #    'move': random.choice(directions),
+    #    'taunt': 'battlesnake-python!'
+    #}
+
     return {
-        'move': random.choice(directions),
+        'move': "left",
         'taunt': 'battlesnake-python!'
+        #if last move equals left, go right
+        if ('move' = "left"): #searching for name of last move?
+            'move'= "right"
+        else 'move' = "left"
     }
 
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, host=os.getenv('IP', '192.168.0.16'), port=os.getenv('PORT', '8080'))
